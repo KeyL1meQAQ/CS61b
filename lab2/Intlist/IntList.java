ccaf59fe421b15cc98b07c09966337de60a59ad7
Bohan Zhang <920060768@qq.com>
2020-12-31 16:29:42 +0800

    Second completed of lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,10 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null | B == null) {
+            return null;
+        }
+
         IntList L = A;
         while (L.rest != null) {
             L = L.rest;
@@ -96,6 +100,10 @@ public class IntList {
      */
     public static IntList catenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if (A == null | B == null) {
+            return null;
+        }
+
         IntList intList = new IntList(A.first, null);
         IntList ptr = intList;
         IntList L = A.rest;
